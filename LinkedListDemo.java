16b450a2caf43adaf6f15efefbd667267e52e586
kiran68 <kiranurshil88@gmail.com>
2023-04-07 00:49:56 +0530

    Add: Using Append In Linked List

--- LinkedListDemo.java
+++ LinkedListDemo.java
@@ -1,50 +1,71 @@
-package com.bridgelabz.linkedlist;
+package com.bridgelabz.linkedlistDemo;
 
-import java.util.LinkedList;
+class Node {
+	int data;
+	Node next;
+
+	public Node(int data) {
+		this.data = data;
+		this.next = null;
+	}
+}
+
+class LinkedList {
+	Node head;
+
+	public LinkedList() {
+		this.head = null;
+	}
+
+	public void append(int data) {
+		Node new_node = new Node(data);
+		if (head == null) {
+			head = new_node;
+		} else {
+			Node current = head;
+			while (current.next != null) {
+				current = current.next;
+			}
+			current.next = new_node;
+		}
+	}
+
+	public void printList() {
+		Node current = head;
+		while (current != null) {
+			System.out.print(current.data + "->");
+			current = current.next;
+		}
+		System.out.println("null");
+	}
+}
 
 public class LinkedListDemo {
-	
-	    public static void main(String[] args) {
-	        // Creating nodes
-	        Node<Integer> node1 = new Node<>(70);
-	        Node<Integer> node2 = new Node<>(30);
-	        Node<Integer> node3 = new Node<>(56);
-	        
-	        // Linking nodes to create a linked list
-	        node1.setNext(node2);
-	        node2.setNext(node3);
-	        
-	        // Printing the linked list
-	        Node<Integer> current = node1;
-	        while (current != null) {
-	            System.out.print(current.getData() + "->");
-	            current = current.getNext();
-	        }
-	        System.out.print("null");
-	    }
+	public static void main(String[] args) {
+		LinkedList myList = new LinkedList();
+		myList.append(56);
+		myList.append(30);
+		myList.append(70);
+
+		myList.printList(); // prints: 56->30->70->null
 	}
+}
 
-	class Node<K> {
-	    private final K data;
-	    private Node<K> next;
 
-	    public Node(K data) {
-	        this.data = data;
-	        this.next = null;
-	    }
 
-	    public K getData() {
-	        return data;
-	    }
 
-	    public void setNext(Node<K> next) {
-	        this.next = next;
-	    }
 
-	    public Node<K> getNext() {
-	        return next;
-	    }
-	}
+
+
+
+
+
+
+
+
+
+
+
 
 
 
