d8e4bea8eb393b5ccd70bd7eab382121df80868f
kiran68 <kiranurshil88@gmail.com>
2023-04-07 11:02:31 +0530

    Add: Insert Into Linked List

--- LinkedListDemo.java
+++ LinkedListDemo.java
@@ -1,15 +1,78 @@
 package com.bridgelabz.linkedlistDemo;
 
-import java.util.LinkedList;
 
-public class LinkedListDemo {
-	
-		public static void main(String[] args) {
-			LinkedList linkedList = new LinkedList();
-			linkedList.add(56);
-			linkedList.add(30);
-			linkedList.add(70);
-			System.out.println("Elements in the list: " + linkedList);
+
+
+
+class Node {
+	int data;
+	Node next;
+
+	public Node(int data) {
+		this.data = data;
+		this.next = null;
+	}
+}
+
+class LinkedList {
+	Node head;
+
+	public LinkedList() {
+		this.head = null;
+	}
+
+	public void append(int data) {
+		Node node1 = new Node(data);
+		if (head == null) {
+			head = node1;
+		} else {
+			Node current = head;
+			while (current.next != null) {
+				current = current.next;
+			}
+			current.next = node1;
 		}
 	}
+
+	public void insert(int data, int afterData) {
+		Node node1 = new Node(data);
+		if (head == null) {
+			head = node1;
+		} else {
+			Node current = head;
+			while (current != null && current.data != afterData) {
+				current = current.next;
+			}
+			if (current != null) {
+				node1.next = current.next;
+				current.next = node1;
+			} else {
+				System.out.println("Node with data " + afterData + " not found");
+			}
+		}
+	}
+
+	public void printList() {
+		Node current = head;
+		while (current != null) {
+			System.out.print(current.data + "->");
+			current = current.next;
+		}
+		System.out.println("null");
+	}
+}
+
+public class LinkedListDemo {
+	public static void main(String[] args) {
+		LinkedList myList = new LinkedList();
+		myList.append(56);
+		myList.append(70);
+
+		myList.insert(30, 56);
+
+		myList.printList(); // prints: 56->30->70->null
+	}
+}
+
+
 
